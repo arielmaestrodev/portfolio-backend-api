e528a6aee374a193aa54f073977ab1c915a626f0
Ariel Batoon <info.arielbatoon@gmail.com>
2026-04-27 10:47:07 +0800

    Added RBAC

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { BlogController } from "@/controllers/blog.controller";
 import { AuthMiddleware } from "@/middlewares/auth-middleware";
+import { permittedRole } from "@/middlewares/rbac-middleware";
+import { Role } from "@/generated/prisma/enums";
 import { validateSchema } from "@/middlewares/validate-schema";
 import { createBlogSchema, updateBlogSchema } from "@/schema/blog";
 
@@ -12,9 +14,9 @@ const auth = new AuthMiddleware();
 router.get("/v1/all-posts", blogController.getAllBlogs);
 router.get("/v1/post/:id", blogController.getBlog);
 
-// Private Routes (Require Authentication)
-router.post("/v1/create-post", auth.execute, validateSchema(createBlogSchema), blogController.createBlog);
-router.post("/v1/update-post", auth.execute, validateSchema(updateBlogSchema), blogController.updateBlog);
-router.delete("/v1/delete-post/:id", auth.execute, blogController.deleteBlog);
+// Private Routes (Require Authentication & Admin Role)
+router.post("/v1/create-post", auth.execute, permittedRole([Role.ADMIN]), validateSchema(createBlogSchema), blogController.createBlog);
+router.post("/v1/update-post", auth.execute, permittedRole([Role.ADMIN]), validateSchema(updateBlogSchema), blogController.updateBlog);
+router.delete("/v1/delete-post/:id", auth.execute, permittedRole([Role.ADMIN]), blogController.deleteBlog);
 
 export default router;
